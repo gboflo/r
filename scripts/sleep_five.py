be1d2b6a031460f2c9e384a3ff76ae6d1383c902
Bocai, Georgiana <georgiana.bocai@spirent.com>
2020-10-19 18:39:05 +0300

    modified sleep5,py

--- scripts/sleep_five.py
+++ scripts/sleep_five.py
@@ -2,7 +2,8 @@
 import sys
 import time
 
-duration = 5
+duration = 7
+print("gimi")
 
 print "[{}]: '{}' sleeping for {} seconds ...".format(time.ctime(), sys.argv[0], duration)
 time.sleep( duration )
