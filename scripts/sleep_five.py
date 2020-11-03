7013824cac50e742f96d4c6f8adab834169bea0e
gboflo <georgiana.bocai@spirent.com>
2020-11-03 14:31:40 +0200

    For demo files

--- scripts/sleep_five.py
+++ scripts/sleep_five.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 duration = 5
-print("gim")
+print("modified")
 #print "[{}]: '{}' sleeping for {} seconds ...".format(time.ctime(), sys.argv[0], duration)
 time.sleep( duration )
 #print "[{}]: '{}' done sleeping for {} seconds".format(time.ctime(), sys.argv[0], duration)
